a28a55d621fe0f2f504273434e1b647a1f0494e9
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-09-29 15:26:39 +0600

    Reload Error Issue Fix.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,12 +12,6 @@ const Navbar = () => {
 			<li>
 				<NavLink to={"/posts"}>Posts</NavLink>
 			</li>
-			<li>
-				<NavLink to={"/gallery"}>Gallery</NavLink>
-			</li>
-			<li>
-				<NavLink to={"/contact"}>Contact</NavLink>
-			</li>
 		</>
 	);
 	return (
